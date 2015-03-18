a44c22e9aac141b1979ff3639031d240d2e13549
kekupua <seteramae@gmail.com>
2015-03-18 13:37:33 -1000

    Added Web View.

--- Early Bird/ViewController.swift
+++ Early Bird/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    @IBOutlet var myWebView: UIWebView!
     
-
+    @IBAction func Menu(sender: AnyObject) {
+        let url = NSURL(string: "http://www.iolani.org/files/pdfs/2015_march_lunch_menus.pdf")
+        let requestObj = NSURLRequest(URL: url!)
+        myWebView.loadRequest(requestObj)
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
